ca9e08d5f19356f57d168cf8e066740214587848
Dani Sarfati <daniel.sarfati@lcecorp.com>
2024-04-09 14:32:28 -0400

    added some basic JS stuff

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-import { _electron as electron, ElectronApplication } from 'playwright-core';
+import { _electron as electron } from 'playwright-core';
 
 const baseURL = 'http://www.amazon.com';
 let homeDir = process.env.HOME;
@@ -33,6 +33,36 @@ test('Open Electron App', async () => {
   // Exit app.
   await electronApp.close();
 });
+
+test('Open Electron App Intentional Failure', async () => {
+  // Launch Electron app.
+  const path = homeDir.concat(
+    '/dani-new-react-app/release/build/mac/ElectronReact.app/Contents/MacOS/ElectronReact',
+  );
+  console.log('Attempting to launch Electron app at: ', path);
+  const electronApp = await electron.launch({ executablePath: path });
+  // // Evaluation expression in the Electron context.
+  // const appPath = await electronApp.evaluate(async ({ app }) => {
+  //   // This runs in the main Electron process, parameter here is always
+  //   // the result of the require('electron') in the main app script.
+  //   return app.getAppPath();
+  // });
+  // console.log(appPath);
+
+  // Get the first window that the app opens, wait if necessary.)
+  const window = await electronApp.firstWindow();
+  // Print the title.
+  console.log(window.title);
+  // Capture a screenshot.
+  await window.screenshot({ path: 'intro.png' });
+  // Direct Electron console to Node terminal.
+  window.on('console', console.log);
+  // Click button.
+  await window.click('text=Nothinggggg');
+  // Exit app.
+  await electronApp.close();
+});
+
 
 test('Amazon has title', async ({ page }) => {
   await page.goto(baseURL);
