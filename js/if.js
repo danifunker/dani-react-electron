55d29a574adf9712e62646540afeb45d96646c75
Dani Sarfati <daniel.sarfati@lcecorp.com>
2024-04-09 14:36:10 -0400

    finalized if js

--- js/if.js
+++ js/if.js
@@ -4,7 +4,7 @@ const function1 = (a, b) => {
   let finalResult = 0;
 
   if (b === 'jackpot') {
-    finalResult = a * 1000;
+    finalResult = a * 90210;
   } else {
     finalResult = a * 100;
   }
@@ -12,5 +12,5 @@ const function1 = (a, b) => {
   return finalResult;
 };
 
-console.log(argv);
+console.log("Thank you for your input, your winnings are:")
 console.log(function1(argv[2], argv[3]));
